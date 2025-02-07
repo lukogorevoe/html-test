0207d3e83d7fe61a39f2c3985c6101519ef7aebd
Marina <example@gmail.com>
2025-02-07 11:19:22 +0700

    Created personalMoviesDB function

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const a = prompt('Один из последних просмотренных ф
     d = prompt('На сколько оцените его?', '');
 
 
-personalMovieDB.movies[a] = b;
+personalMovieDB.movies[a] = b;s
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
